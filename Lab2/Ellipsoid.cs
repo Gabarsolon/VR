509345f1f12ceac207a7d18ad47a992d5a1f1b4f
917_Turcu_Remus <78847479+Gabarsolon@users.noreply.github.com>
2023-11-15 23:40:26 +0200

    LAB-2: Refactorized code

--- Lab2/Ellipsoid.cs
+++ Lab2/Ellipsoid.cs
@@ -3,83 +3,73 @@
 
 namespace rt
 {
-    public class Ellipsoid : Geometry
-    {
-        private Vector Center { get; }
-        private Vector SemiAxesLength { get; }
-        private double Radius { get; }
-        
-        
-        public Ellipsoid(Vector center, Vector semiAxesLength, double radius, Material material, Color color) : base(material, color)
-        {
-            Center = center;
-            SemiAxesLength = semiAxesLength;
-            Radius = radius;
-        }
+	public class Ellipsoid : Geometry
+	{
+		private Vector Center { get; }
+		private Vector SemiAxesLength { get; }
+		private double Radius { get; }
 
-        public Ellipsoid(Vector center, Vector semiAxesLength, double radius, Color color) : base(color)
-        {
-            Center = center;
-            SemiAxesLength = semiAxesLength;
-            Radius = radius;
-        }
 
-        public Vector Normal(Vector point)
-        {
+		public Ellipsoid(Vector center, Vector semiAxesLength, double radius, Material material, Color color) : base(material, color)
+		{
+			Center = center;
+			SemiAxesLength = semiAxesLength;
+			Radius = radius;
+		}
+
+		public Ellipsoid(Vector center, Vector semiAxesLength, double radius, Color color) : base(color)
+		{
+			Center = center;
+			SemiAxesLength = semiAxesLength;
+			Radius = radius;
+		}
+
+		private Vector Normal(Vector point)
+		{
 			return new Vector(
-			   2 * (point.X - center.X) / (A * A),
-			   2 * (point.Y - center.Y) / (B * B),
-			   2 * (point.Z - center.Z) / (C * C)
-		   ).Normalize();
+				2 * (point.X - Center.X) / SemiAxesLength.X,
+				2 * (point.Y - Center.Y) / SemiAxesLength.Y,
+				2 * (point.Z - Center.Z) / SemiAxesLength.Z
+			).Normalize();
 		}
 
 		public override Intersection GetIntersection(Line line, double minDist, double maxDist)
 		{
-			// Transform the line to the ellipsoid's local space
-			Vector localX0 = line.X0 - Center;
-			Vector localDx = line.Dx;
+			var a = line.Dx.X;
+			var b = line.X0.X;
+			var c = line.Dx.Y;
+			var d = line.X0.Y;
+			var e = line.Dx.Z;
+			var f = line.X0.Z;
 
-			// Calculate coefficients for the quadratic equation
-			double a = (localDx.X * localDx.X) / (SemiAxesLength.X * SemiAxesLength.X) +
-					   (localDx.Y * localDx.Y) / (SemiAxesLength.Y * SemiAxesLength.Y) +
-					   (localDx.Z * localDx.Z) / (SemiAxesLength.Z * SemiAxesLength.Z);
+			var A_squared = SemiAxesLength.X * SemiAxesLength.X;
+			var B_squared = SemiAxesLength.Y * SemiAxesLength.Y;
+			var C_squared = SemiAxesLength.Z * SemiAxesLength.Z;
 
-			double b = 2 * (localDx.X * localX0.X) / (SemiAxesLength.X * SemiAxesLength.X) +
-					   2 * (localDx.Y * localX0.Y) / (SemiAxesLength.Y * SemiAxesLength.Y) +
-					   2 * (localDx.Z * localX0.Z) / (SemiAxesLength.Z * SemiAxesLength.Z);
-
-			double c = (localX0.X * localX0.X) / (SemiAxesLength.X * SemiAxesLength.X) +
-					   (localX0.Y * localX0.Y) / (SemiAxesLength.Y * SemiAxesLength.Y) +
-					   (localX0.Z * localX0.Z) / (SemiAxesLength.Z * SemiAxesLength.Z) - Radius*Radius;
-
-			// Solve the quadratic equation
-			double discriminant = (b * b) - (4.0 * a * c);
-
-			if (discriminant < 0.0)
+			var ap = a * a / A_squared + c * c / B_squared + e * e / C_squared;
+			var bp = 2 * (a * (b - Center.X) / A_squared + c * (d - Center.Y) / B_squared + e * (f - Center.Z) / C_squared);
+			var cp = (b - Center.X) * (b - Center.X) / A_squared + (d - Center.Y) * (d - Center.Y) / B_squared + (f - Center.Z) * (f - Center.Z) / C_squared - Radius * Radius;
+			var delta = bp * bp - 4 * ap * cp;
+			if (delta < 0.0001)
 			{
-				// No intersection
-				return new Intersection(false, false, this, line, 0, new Vector());
+				return new Intersection(false, false, this, line, 0, null);
 			}
+			var t1 = (-bp - Math.Sqrt(bp * bp - 4 * ap * cp)) / (2 * ap);
+			var t2 = (-bp + Math.Sqrt(bp * bp + 4 * ap * cp)) / (2 * ap);
 
-			double t1 = (-b - Math.Sqrt(discriminant)) / (2.0 * a);
-			double t2 = (-b + Math.Sqrt(discriminant)) / (2.0 * a);
+			var t1verif = minDist <= t1 && t1 <= maxDist;
+			var t2verif = minDist <= t2 && t2 <= maxDist;
 
-			if (t1 > maxDist || t2 < minDist)
-			{
-				// Intersection points are outside the specified range
-				return new Intersection(false, false, this, line, 0, new Vector());
-			}
+			if (t1verif == false && t2verif == false)
+				return new Intersection(false, false, this, line, 0, null);
+			if (t1verif == false && t2verif == true)
+				return new Intersection(true, true, this, line, t2, Normal(line.Dx * t2 + line.X0));
+			if (t2verif == false && t1verif == true)
+				return new Intersection(true, true, this, line, t1, Normal(line.Dx * t1 + line.X0));
 
-			double t = Math.Max(t1, minDist);
-
-			// Calculate the intersection point in world space
-			Vector intersectionPoint = localX0 + localDx * t;
-
-			// Calculate the normal vector at the intersection point
-			Vector normal = Normal(intersectionPoint);
-
-			return new Intersection(true, true, this, line, t, normal);
+			if (t1 < t2)
+				return new Intersection(true, true, this, line, t1, Normal(line.Dx * t1 + line.X0));
+			return new Intersection(true, true, this, line, t2, Normal(line.Dx * t2 + line.X0));
 		}
-
 	}
 }
